8580de7160535b0fba6416919ec16475b3305021
Suzii <z.dankovcikova@gmail.com>
2015-10-05 21:27:38 +0200

    Coding rules

---
+++ CodingRules.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class CodingRules
+{
+    public const string NAME_CONSTANT = "Someone";  //constants are written using uppercase and underscore as a separator
+
+    private string _field;    //private members of the class are prefixed with an uderscore
+
+    public int MyProperty { get; set; }    //public properties are written using UpperCamelCase
+
+    //all the method names use UpperCamelCase
+    public string Hello(string name)
+    {
+        //always use blocks, even for one line
+        if (name == null)
+        {
+            throw new ArgumentNullException();
+        }
+
+        //variables use lowerCamelCase
+        var myName = name;
+
+        return myName;
+    }
+}
