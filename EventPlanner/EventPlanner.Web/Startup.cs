bc7a551ef56358b8595653955aa1f429933d6965
JayDee8 <jirka.dockal.8@seznam.cz>
2015-10-08 20:44:57 +0200

    New solution with web project

---
+++ EventPlanner/EventPlanner.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventPlanner.Web.Startup))]
+namespace EventPlanner.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
