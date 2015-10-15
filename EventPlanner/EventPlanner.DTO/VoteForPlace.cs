31f1b464067a11b2e4f68f146d637988334db2b7
JayDee8 <jirka.dockal.8@seznam.cz>
2015-10-15 22:05:55 +0200

    DB context prepare

---
+++ EventPlanner/EventPlanner.DTO/VoteForPlace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace EventPlanner.Models.Domain
+{
+    public class VoteForPlace
+    {
+        public Guid Id { set; get; }
+        public Guid UserId { set; get; }
+        public Guid PlaceId { set; get; }
+    }
+}
--- EventPlanner/EventPlanner.Models/Domain/Event.cs
+++ EventPlanner/EventPlanner.Models/Domain/Event.cs
@@ -1,19 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EventPlanner.Models.Domain
 {
+    [Table("Events")]
     public class Event
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public Guid Id { get; set; }
+
+        [Required]
         public string Title { get; set; }
+
         public string Desc { get; set; }
+
         public float ExpectedLength { get; set; }
+
+        [Required]
         public Guid OrganizerId { get; set; }
+
         public bool OthersCanEdit { get; set; }
+
+        [Required]
         public DateTime Created { get; set; }
+        
         public IEnumerable<Place> Places { get; set; }
+
         public IEnumerable<TimeSlot> TimeSlots { get; set; }
+
+        [Required]
         public int Hash
         {
             get
