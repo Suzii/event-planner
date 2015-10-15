a1d84b28284d30c94bd2fe0802fff6e8d0feaa98
JayDee8 <jirka.dockal.8@seznam.cz>
2015-10-15 23:14:13 +0200

    DB context change , dto required annotation added

--- EventPlanner/EventPlanner.DTO/Place.cs
+++ EventPlanner/EventPlanner.DTO/Place.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EventPlanner.Models.Domain
 {
     public class Place
     {
         public Guid Id { set; get; }
+
+        [Required]
         public Guid EventId { set; get; }
+
+        [Required]
         public Guid VenueId { set; get; }
+
         public IEnumerable<VoteForPlace> VotesForPlace { set; get; }
 
     }
