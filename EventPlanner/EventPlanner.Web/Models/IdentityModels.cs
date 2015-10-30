a7f4a564e27f53a867e000124c7696d6cf109006
JayDee8 <jirka.dockal.8@seznam.cz>
2015-10-30 01:11:27 +0100

    still fix

--- EventPlanner/EventPlanner.Web/Models/IdentityModels.cs
+++ EventPlanner/EventPlanner.Web/Models/IdentityModels.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using EventPlanner.Entities;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
@@ -19,23 +18,23 @@ namespace EventPlanner.Web.Models
         }
     }
 
-    public class ApplicationDbContext : IdentityDbContext<UserEntity>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
             : base("SqlConnectionString", throwIfV1Schema: false) // was default DefaultConnection
         {
         }
 
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder); // This needs to go before the other rules!
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder); // This needs to go before the other rules!
 
-        //    modelBuilder.Entity<UserEntity>().ToTable("Users");
-        //    modelBuilder.Entity<IdentityRole>().ToTable("Roles");
-        //    modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
-        //    modelBuilder.Entity<IdentityUserClaim>().ToTable("UsersClaims");
-        //    modelBuilder.Entity<IdentityUserLogin>().ToTable("UsersLogins");
-        //}
+            modelBuilder.Entity<ApplicationUser>().ToTable("Users");
+            modelBuilder.Entity<IdentityRole>().ToTable("Roles");
+            modelBuilder.Entity<IdentityUserRole>().ToTable("UserRoles");
+            modelBuilder.Entity<IdentityUserClaim>().ToTable("UsersClaims");
+            modelBuilder.Entity<IdentityUserLogin>().ToTable("UsersLogins");
+        }
 
         public static ApplicationDbContext Create()
         {
