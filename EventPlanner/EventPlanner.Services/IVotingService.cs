b246afde91aa636f0cd0071362bf3a8949b4810a
tomaskristof <kristof.tomas@yahoo.com>
2015-11-04 23:51:51 +0100

    IVotingService - renamed methods

--- EventPlanner/EventPlanner.Services/IVotingService.cs
+++ EventPlanner/EventPlanner.Services/IVotingService.cs
@@ -7,16 +7,16 @@ namespace EventPlanner.Services
 {
     public interface IVotingService
     {
-        Task<IList<VoteForDateEntity>> GetVotesForDates(Guid eventId);
+        Task<IList<VoteForDateEntity>> GetVotesForDatesAsync(Guid eventId);
 
-        Task<IList<VoteForPlaceEntity>> GetVotesForPlaces(Guid eventId);
+        Task<IList<VoteForPlaceEntity>> GetVotesForPlacesAsync(Guid eventId);
 
-        Task<IList<VoteForDateEntity>> GetVotesForDates(Guid eventId, IList<Guid> dateIds);
+        Task<IList<VoteForDateEntity>> GetVotesForDatesAsync(Guid eventId, IList<Guid> dateIds);
 
-        Task<IList<VoteForPlaceEntity>> GetVotesForPlaces(Guid eventId, IList<Guid> placeIds);
+        Task<IList<VoteForPlaceEntity>> GetVotesForPlacesAsync(Guid eventId, IList<Guid> placeIds);
 
-        Task SubmitDateVoteBy(Guid personId, IList<Guid> timeSlotIds);
+        Task SubmitDateVoteByAsync(Guid personId, IList<Guid> timeSlotIds);
 
-        Task SubmitPlaceVoteBy(Guid personId, IList<Guid> placeIds);
+        Task SubmitPlaceVoteByAsync(Guid personId, IList<Guid> placeIds);
     }
 }
--- EventPlanner/EventPlanner.Services/Implementation/VotingService.cs
+++ EventPlanner/EventPlanner.Services/Implementation/VotingService.cs
@@ -7,32 +7,32 @@ namespace EventPlanner.Services.Implementation
 {
     public class VotingService : IVotingService
     {
-        public Task<IList<VoteForDateEntity>> GetVotesForDates(Guid eventId)
+        public Task<IList<VoteForDateEntity>> GetVotesForDatesAsync(Guid eventId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IList<VoteForPlaceEntity>> GetVotesForPlaces(Guid eventId)
+        public Task<IList<VoteForPlaceEntity>> GetVotesForPlacesAsync(Guid eventId)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IList<VoteForDateEntity>> GetVotesForDates(Guid eventId, IList<Guid> dateIds)
+        public Task<IList<VoteForDateEntity>> GetVotesForDatesAsync(Guid eventId, IList<Guid> dateIds)
         {
             throw new NotImplementedException();
         }
 
-        public Task<IList<VoteForPlaceEntity>> GetVotesForPlaces(Guid eventId, IList<Guid> placeIds)
+        public Task<IList<VoteForPlaceEntity>> GetVotesForPlacesAsync(Guid eventId, IList<Guid> placeIds)
         {
             throw new NotImplementedException();
         }
 
-        public Task SubmitDateVoteBy(Guid personId, IList<Guid> timeSlotIds)
+        public Task SubmitDateVoteByAsync(Guid personId, IList<Guid> timeSlotIds)
         {
             throw new NotImplementedException();
         }
 
-        public Task SubmitPlaceVoteBy(Guid personId, IList<Guid> placeIds)
+        public Task SubmitPlaceVoteByAsync(Guid personId, IList<Guid> placeIds)
         {
             throw new NotImplementedException();
         }
