2f848080b96aae78ece440edc85e2795f69573ab
janka <jani.michalkova@gmail.com>
2015-11-06 01:19:25 +0100

    Added cute little map.

---
+++ EventPlanner/EventPlanner.Web/Scripts/Custom/GoogleMapsModule.js
@@ -0,0 +1,13 @@
+﻿function initialize() {
+    var mapCanvas = document.getElementById('map');
+
+    var mapOptions = {
+        center: new google.maps.LatLng(49.197947, 16.607823),
+        zoom: 12,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
