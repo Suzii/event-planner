cd0cc933db4320d2b7a8f49ad62ba77dd1522dc3
janka <jani.michalkova@gmail.com>
2015-11-06 03:54:26 +0100

    Upgraded Home page.

--- EventPlanner/EventPlanner.Web/Controllers/HomeController.cs
+++ EventPlanner/EventPlanner.Web/Controllers/HomeController.cs
@@ -1,13 +1,36 @@
-﻿using System.Web.Mvc;
+﻿using AutoMapper;
+using EventPlanner.Models.Domain;
+using EventPlanner.Models.Models;
+using EventPlanner.Services.Implementation;
+using Microsoft.AspNet.Identity;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
 
 namespace EventPlanner.Web.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        private UserService _userService;
+
+        public HomeController()
         {
-            return View("Index");
+            _userService = new UserService();
+        }
+
+        public async Task<ActionResult> Index()
+        {
+            var result = await _userService.GetEventsCreatedBy(User.Identity.GetUserId());
+            IList<EventModel> model = new List<EventModel>();
+
+            foreach (Event e in result)
+            {
+                model.Add(Mapper.Map<EventModel>(e));
+            }
+
+            return View("Index", new MyEventsViewModel { Events = model });
         }
         
         public ActionResult About()
