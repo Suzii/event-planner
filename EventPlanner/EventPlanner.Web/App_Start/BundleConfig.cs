5ebcaeac260a4134c1b3b77e80f1515ef50160ae
Suzii <z.dankovcikova@gmail.com>
2015-11-17 17:47:43 +0100

    MVC Refactoring : Javascript includes on Event page.

--- EventPlanner/EventPlanner.Web/App_Start/BundleConfig.cs
+++ EventPlanner/EventPlanner.Web/App_Start/BundleConfig.cs
@@ -25,8 +25,8 @@ namespace EventPlanner.Web
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap-datepicker.js",
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap-datepicker.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
