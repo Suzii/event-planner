555d35e1b43c41d73c141efde24c8b3373c0a4dd
Suzii <z.dankovcikova@gmail.com>
2015-11-17 17:15:57 +0100

    Updated method of DBTest controller to test update of event and its related entities.

--- EventPlanner/EventPlanner.Web/Controllers/DbTestController.cs
+++ EventPlanner/EventPlanner.Web/Controllers/DbTestController.cs
@@ -8,6 +8,7 @@ using EventPlanner.Models.Domain;
 using EventPlanner.Models.Models.CreateAndEdit;
 using EventPlanner.Models.Models.Shared;
 using Microsoft.AspNet.Identity;
+using Newtonsoft.Json;
 
 namespace EventPlanner.Web.Controllers
 {
@@ -15,33 +16,49 @@ namespace EventPlanner.Web.Controllers
     [Authorize]
     public class DbTestController : Controller
     {
-        public async Task<JsonResult> Test()
+        public async Task<JsonResult> Index()
         {
             var dates = new List<TimeSlot>();
             dates.Add(new TimeSlot() { DateTime = DateTime.Now });
 
             // NOTE : Dates will not be stored as there is still an issue with mapping
-            EventRepository e = new EventRepository();
             var eventModel = new EventModel()
             {
                 Title = "Some fake event for testing purposes",
                 Desc = "Hello there, we are going to drink some beer! Cheers!",
                 OthersCanEdit = true,
                 ExpectedLength = 2,
-                Places = new List<FourSquareVenueModel>() { new FourSquareVenueModel() { VenueId = "529ebe0f498eee32aa9dee7e" }} ,
+                Places = new List<FourSquareVenueModel>() { new FourSquareVenueModel() { VenueId = "1" }, new FourSquareVenueModel() { VenueId = "2 - to be deleted" }, new FourSquareVenueModel() { VenueId = "3" } } ,
                 Dates = new List<EventModel.DatesModel>() { new EventModel.DatesModel() {Date = DateTime.Today, Times = new List<string>() {"12:00", "13:30"} } }
             };
 
+            // 1. Store event for the first time
+            EventRepository e = new EventRepository();
             var ev = Mapper.Map<Event>(eventModel);
             ev.OrganizerId = User.Identity.GetUserId();
             ev.CreatedOn = DateTime.Today;
-
             ev = await e.AddOrUpdate(ev);
 
-            EventRepository er = new EventRepository();
-            var evFromDb = await er.GetFullEvent(ev.Id);
+            // 2. get event from db
+            e = new EventRepository();
+            var evFromDb = await e.GetFullEvent(ev.Id);
             eventModel = Mapper.Map<Event, EventModel>(evFromDb);
 
+            // 3. update related entities and store again
+            eventModel.Places.RemoveAt(2);
+            eventModel.Places.Add(new FourSquareVenueModel() { VenueId = "4 - newly added" });
+            eventModel.Desc = "Should contain places 1, 3, 4 - newly added";
+            var evEdited = Mapper.Map<Event>(eventModel);
+            evEdited.OrganizerId = ev.OrganizerId;
+            evEdited.CreatedOn = ev.CreatedOn;
+            e = new EventRepository();
+            evFromDb = await e.AddOrUpdate(evEdited);
+
+            // 4. load updated event
+            evFromDb = await e.GetFullEvent(evFromDb.Id);
+            eventModel = Mapper.Map<Event, EventModel>(evFromDb);
+
+            // print out
             return Json(eventModel, JsonRequestBehavior.AllowGet);
         }
 
