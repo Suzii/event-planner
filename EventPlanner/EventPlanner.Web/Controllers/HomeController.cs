1e92ca4c65dc3ea9c39686c5384192790afc2da8
Suzii <z.dankovcikova@gmail.com>
2015-11-17 15:04:35 +0100

    MVC Refactoring : HomeController

--- EventPlanner/EventPlanner.Web/Controllers/HomeController.cs
+++ EventPlanner/EventPlanner.Web/Controllers/HomeController.cs
@@ -1,12 +1,8 @@
 ﻿using AutoMapper;
-using EventPlanner.Models.Domain;
 using EventPlanner.Models.Models.Home;
 using EventPlanner.Services.Implementation;
 using Microsoft.AspNet.Identity;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Net;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Web.Mvc;
 
@@ -15,30 +11,18 @@ namespace EventPlanner.Web.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        private UserService _userService;
-        private EventManagementService _eventService;
+        private readonly UserService _userService;
 
         public HomeController()
         {
             _userService = new UserService();
-            _eventService = new EventManagementService();
         }
 
         public async Task<ActionResult> Index()
         {
-            var result = await _userService.GetEventsCreatedBy(User.Identity.GetUserId());
-            IList<EventViewModel> model = new List<EventViewModel>();
-
-            foreach (Event e in result)
-            {
-                model.Add(Mapper.Map<EventViewModel>(e));
-            }
-
-            return View("Index", new MyEventsViewModel { Events = model });
+            return View("Index", await ConstructModel(User.Identity.GetUserId()));
         }
-
         
-
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -51,6 +35,13 @@ namespace EventPlanner.Web.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        private async Task<MyEventsViewModel> ConstructModel(string userId)
+        {
+            var result = await _userService.GetEventsCreatedBy(userId);
+            var events = result.Select(e => Mapper.Map<EventViewModel>(e)).ToList();
+            return new MyEventsViewModel {Events = events};
         }
     }
 }
