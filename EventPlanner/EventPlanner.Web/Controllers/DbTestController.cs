764fc2f3a264e425ec7d4e2a5cacf9a7cd55f84e
tomaskristof <kristof.tomas@yahoo.com>
2015-11-19 12:48:14 +0100

    Fixed saving related entities (right now just places) when updating an Event. Only draft - will be refactored

--- EventPlanner/EventPlanner.Web/Controllers/DbTestController.cs
+++ EventPlanner/EventPlanner.Web/Controllers/DbTestController.cs
@@ -7,8 +7,8 @@ using EventPlanner.DAL.Repository;
 using EventPlanner.Models.Domain;
 using EventPlanner.Models.Models.CreateAndEdit;
 using EventPlanner.Models.Models.Shared;
+using EventPlanner.Services.Implementation;
 using Microsoft.AspNet.Identity;
-using Newtonsoft.Json;
 
 namespace EventPlanner.Web.Controllers
 {
@@ -18,8 +18,7 @@ namespace EventPlanner.Web.Controllers
     {
         public async Task<JsonResult> Index()
         {
-            var dates = new List<TimeSlot>();
-            dates.Add(new TimeSlot() { DateTime = DateTime.Now });
+            var ems = new EventManagementService();
 
             // NOTE : Dates will not be stored as there is still an issue with mapping
             var eventModel = new EventModel()
@@ -32,31 +31,34 @@ namespace EventPlanner.Web.Controllers
                 Dates = new List<EventModel.DatesModel>() { new EventModel.DatesModel() { Date = DateTime.Today, Times = new List<EventModel.TimeModel>() { new EventModel.TimeModel() {Time = "10:00:00"}, new EventModel.TimeModel() {Time = "12:00:00"} } } }
             };
 
+            var e = Mapper.Map<Event>(eventModel);
             // 1. Store event for the first time
-            EventRepository e = new EventRepository();
-            var ev = Mapper.Map<Event>(eventModel);
-            ev.OrganizerId = User.Identity.GetUserId();
-            ev.CreatedOn = DateTime.Today;
-            ev = await e.AddOrUpdate(ev);
+            e = await ems.CreateEventAsync(e, User.Identity.GetUserId());
+
+            eventModel = Mapper.Map<EventModel>(e);
 
             // 2. get event from db
-            e = new EventRepository();
-            var evFromDb = await e.GetFullEvent(ev.Id);
-            eventModel = Mapper.Map<Event, EventModel>(evFromDb);
+            //e = new EventRepository();
+            //var evFromDb = await e.GetFullEvent(ev.Id);
+            //eventModel = Mapper.Map<Event, EventModel>(evFromDb);
 
             // 3. update related entities and store again
-            eventModel.Places.RemoveAt(2);
-            eventModel.Places.Add(new FourSquareVenueModel() { VenueId = "4 - newly added" });
-            eventModel.Desc = "Should contain places 1, 3, 4 - newly added";
-            var evEdited = Mapper.Map<Event>(eventModel);
-            evEdited.OrganizerId = ev.OrganizerId;
-            evEdited.CreatedOn = ev.CreatedOn;
-            e = new EventRepository();
-            evFromDb = await e.AddOrUpdate(evEdited);
+            eventModel.Places.RemoveAt(1); // do we have to handle this manually in service?
+            eventModel.Places[0].VenueId += "(edited)";
+            eventModel.Places.Add(new FourSquareVenueModel() { VenueId = "4 - newly added"/*, EventId = eventModel.Id.Value*/});
+            eventModel.Desc = "Should contain places 1(edited), 3, 4 - newly added";
+            //var evEdited = Mapper.Map<Event>(eventModel);
+            //evEdited.OrganizerId = ev.OrganizerId;
+            //evEdited.CreatedOn = ev.CreatedOn;
+            //e = new EventRepository();
+            //evFromDb = await e.AddOrUpdate(evEdited);
+            e = Mapper.Map<Event>(eventModel);
+            e = await ems.UpdateEventAsync(e);
+            eventModel = Mapper.Map<EventModel>(e);
 
             // 4. load updated event
-            evFromDb = await e.GetFullEvent(evFromDb.Id);
-            eventModel = Mapper.Map<Event, EventModel>(evFromDb);
+            //evFromDb = await e.GetFullEvent(evFromDb.Id);
+            //eventModel = Mapper.Map<Event, EventModel>(evFromDb);
 
             // print out
             return Json(eventModel, JsonRequestBehavior.AllowGet);
