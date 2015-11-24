404dbffb1e2ca4f66c4177753fcc89952fc890eb
tomaskristof <kristof.tomas@yahoo.com>
2015-11-25 00:34:30 +0100

    Facebook registration - get user name from fb

--- EventPlanner/EventPlanner.Web/Controllers/AccountController.cs
+++ EventPlanner/EventPlanner.Web/Controllers/AccountController.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Globalization;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using System.Web;
@@ -342,9 +340,12 @@ namespace EventPlanner.Web.Controllers
                 case SignInStatus.Failure:
                 default:
                     // If the user does not have an account, then prompt the user to create an account
+                    ClaimsIdentity ext = await AuthenticationManager.GetExternalIdentityAsync(DefaultAuthenticationTypes.ExternalCookie);
+                    //var emailClaim = ext.Result.Claims.FirstOrDefault(c => c.Type == ClaimTypes.Email);
+                    //var email = emailClaim.Value;
                     ViewBag.ReturnUrl = returnUrl;
                     ViewBag.LoginProvider = loginInfo.Login.LoginProvider;
-                    return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = loginInfo.Email });
+                    return View("ExternalLoginConfirmation", new ExternalLoginConfirmationViewModel { Email = loginInfo.Email, Name = ext.Name});
             }
         }
 
