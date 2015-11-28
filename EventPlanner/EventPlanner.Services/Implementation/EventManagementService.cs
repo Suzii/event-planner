5a0952be609cc6c516fb2c42c43f3d808fd4604c
tomaskristof <kristof.tomas@yahoo.com>
2015-11-28 12:30:30 +0100

    EventManagementService documentation

--- EventPlanner/EventPlanner.Services/Implementation/EventManagementService.cs
+++ EventPlanner/EventPlanner.Services/Implementation/EventManagementService.cs
@@ -7,16 +7,29 @@ using EventPlanner.Models.Domain;
 
 namespace EventPlanner.Services.Implementation
 {
+    /// <summary>
+    /// Class providing services to manage events
+    /// </summary>
     public class EventManagementService : IEventManagementService
     {
         private readonly EventRepository _eventRepository;
         private readonly PlaceRepository _placeRepository;
         private readonly TimeSlotRepository _timeSlotRepository;
 
+        /// <summary>
+        /// Initialize new instance of EventManagementService
+        /// </summary>
         public EventManagementService() : this(new EventRepository(), new PlaceRepository(), new TimeSlotRepository())
         {
+
         }
 
+        /// <summary>
+        /// Initialize new instance of EventManagementService
+        /// </summary>
+        /// <param name="eventRepository">EventRepository to operate with</param>
+        /// <param name="placeRepository">PlaceRepository to operate with</param>
+        /// <param name="timeSlotRepository">TomeSlotRepository to operate with</param>
         public EventManagementService(EventRepository eventRepository, PlaceRepository placeRepository,
             TimeSlotRepository timeSlotRepository)
         {
@@ -25,6 +38,12 @@ namespace EventPlanner.Services.Implementation
             _timeSlotRepository = timeSlotRepository;
         }
 
+        /// <summary>
+        /// Create new event and stores it to database in async operation.
+        /// </summary>
+        /// <param name="e">Event to create. Event should not have filled Id</param>
+        /// <param name="userId">Id of user who is creating this event</param>
+        /// <returns>Created event with generated Id</returns>
         public async Task<Event> CreateEventAsync(Event e, string userId)
         {
             e.OrganizerId = userId;
@@ -32,6 +51,11 @@ namespace EventPlanner.Services.Implementation
             return await _eventRepository.AddOrUpdate(e);
         }
 
+        /// <summary>
+        /// Update event and stores it to database in async operation. Places and TimeSlots which were removed from this Event are deleted from database with its votes 
+        /// </summary>
+        /// <param name="e">Event to update</param>
+        /// <returns>Updated event</returns>
         public async Task<Event> UpdateEventAsync(Event e)
         {
             var fullEvent = await GetFullEventAsync(e.Id);
@@ -55,6 +79,10 @@ namespace EventPlanner.Services.Implementation
             return await _eventRepository.AddOrUpdate(e);
         }
 
+        /// <summary>
+        /// Disable event in async operation
+        /// </summary>
+        /// <param name="id">Event Id</param>
         public async Task DisableEventAsync(Guid id)
         {
             var e = await _eventRepository.GetEventInfo(id);
@@ -62,16 +90,31 @@ namespace EventPlanner.Services.Implementation
             await _eventRepository.AddOrUpdate(e);
         }
 
+        /// <summary>
+        /// Get full event details with places and timeslots
+        /// </summary>
+        /// <param name="id">Event Id</param>
+        /// <returns>Event with full details</returns>
         public async Task<Event> GetFullEventAsync(Guid id)
         {
             return await _eventRepository.GetFullEvent(id);
         }
 
+        /// <summary>
+        /// Get basic event details without places and timeslots
+        /// </summary>
+        /// <param name="id">Event Id</param>
+        /// <returns>Basic event details without Places and Timeslots</returns>
         public async Task<Event> GetEventInfoAsync(Guid id)
         {
             return await _eventRepository.GetEventInfo(id);
         }
 
+        /// <summary>
+        /// Decode EventHash to its Id
+        /// </summary>
+        /// <param name="eventHash">Hash of an event</param>
+        /// <returns>Id of event</returns>
         public Guid GetEventId(string eventHash)
         {
             eventHash = eventHash.Replace("_", "/");
