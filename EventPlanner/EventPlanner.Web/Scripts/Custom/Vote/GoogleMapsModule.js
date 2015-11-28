d75a1647a492bed5622353f217bbed5613d39560
janka <jani.michalkova@gmail.com>
2015-11-28 04:09:02 +0100

    Map with self-focus on markers and Info bubbles on click.

--- EventPlanner/EventPlanner.Web/Scripts/Custom/Vote/GoogleMapsModule.js
+++ EventPlanner/EventPlanner.Web/Scripts/Custom/Vote/GoogleMapsModule.js
@@ -7,12 +7,31 @@
         mapTypeId: google.maps.MapTypeId.ROADMAP
     }
 
-    var map = new google.maps.Map(mapCanvas, mapOptions)
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    
+    var bounds = new google.maps.LatLngBounds();
+
+    map.data.addListener('addfeature', function (data) {
+        bounds.extend(data.feature.getGeometry().get());
+        map.fitBounds(bounds);
+    });
     
     map.data.loadGeoJson($("#map").attr("data-places"));
+        
+    var infoWindow = new google.maps.InfoWindow();
+    map.data.addListener('click', function (data) {
+        infoWindow.setContent(
+            '<div class="text-primary header" style="font-weight: normal">' + data.feature.getProperty('name') + '</div>' +
+            '<div class="text-muted">' + data.feature.getProperty('address') + '</div>'
+            );
+        infoWindow.setPosition(data.feature.getGeometry().get());
+        infoWindow.open(map);
+    });
+
     map.data.setStyle({
         icon: './../../../Content/Images/point.png'
     });
 }
+
 
 google.maps.event.addDomListener(window, 'load', initialize);
